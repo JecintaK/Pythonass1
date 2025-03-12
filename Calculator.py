336ab2886e104129ffaa6b171f7db0738a66ac23
JecintaK <163906476+JecintaK@users.noreply.github.com>
2025-03-12 04:54:51 -0700

    Add files via upload
    A simple calculator that operators arithmetic functions on the user input.

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+num1=float(input('Enter the first number: '))
+print(num1)
+num2=float(input('Enter the second number: '))
+print(num2)
+addition = num1 + num2
+print(f"addition  {addition} ")
+subtraction = num1-num2
+print(f"subtraction {subtraction}" )
+multiply = num1*num2
+print(f"multiplication {multiply}")
+divide = num1/num2
+print(f"division {divide}")
